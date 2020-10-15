e7f2feeffe3c216070ea7308d3b87c1efdf86a9f
Jonathan Cobb <jmc846@scarletmail.rutgers.edu>
2020-10-15 00:16:26 -0400

    installed react

---
+++ react-directory/components/footer.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+function App() {
+  const [developerState, setDeveloperState] = useState({
+    excitementLevel: 10000,
+    lifeLongLearner: true,
+    mood: "excited",
+    name: "Alec"
+  });
